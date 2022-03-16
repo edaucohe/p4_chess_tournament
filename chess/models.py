73ce2f4432ecc1d187496a5c6244696577c4d7b3
Antoine Latrille <antoine.latrille@gmail.com>
2022-03-16 14:01:11 +0100

    feat(models): cleanup models to be on the right track and explain serialization and de-serialization for future use with the DB

--- chess/models.py
+++ chess/models.py
@@ -1,6 +1,7 @@
+import dataclasses
 from datetime import date, datetime
 from enum import Enum
-from typing import List, Optional, Tuple, Dict, Union
+from typing import Any, List, Optional, Tuple, Dict, Union
 from dataclasses import dataclass, field
 
 DEFAULT_PLAYERS_NUMBER = 8
@@ -18,13 +19,34 @@ class Sex(Enum):
     FEMALE = 'f'
 
 
-@dataclass
+@dataclass(unsafe_hash=True)
 class Player:
     first_name: str
     last_name: str
     date_of_birth: date
     sex: Sex
     ranking: int
+
+    def to_json(self) -> Dict[str, Any]:
+        # Serialization
+        player_as_dict = dataclasses.asdict(self)
+        player_as_dict['date_of_birth'] = self.date_of_birth.isoformat()
+        player_as_dict['sex'] = self.sex.value
+        return player_as_dict
+
+    @classmethod
+    def from_json(cls, data: Dict[str, Any]) -> 'Player':
+        # Deserialization
+        data['date_of_birth'] = date.fromisoformat(data['date_of_birth'])
+        data['sex'] = Sex(data['sex'])
+
+        return Player(**data)
+
+# p1 => p1_as_dict => p1_as_dict_as_json
+# Pour la db, on veut passer de :
+# player = Player('name', 'lastname', date.today(), Sex.FEMALE, 1)
+# à
+# player = {'first_name': 'name', 'last_name': 'lastname', 'date_of_birth': '2022-03-16', 'sex': 'f', 'ranking': 1}
 
 
 class MatchResult(Enum):
@@ -33,42 +55,17 @@ class MatchResult(Enum):
     DRAW = 0.5
 
 
-# class PlayerScore:
-#     player_score: Tuple[Player, Optional[MatchResult]] = (Player, field(default=SCORE_INIT))
-
-
-# Match = Tuple[List[Player, Optional[MatchResult]], List[Player, Optional[MatchResult]]]
-# match: Tuple[Tuple[Player, Optional[MatchResult]], Tuple[Player, Optional[MatchResult]]]
-# Match = Tuple[List[Union[Player, Optional[MatchResult]]], List[Union[Player, Optional[MatchResult]]]]
-# match: Tuple[Tuple[PlayerScore], Tuple[PlayerScore]]
-# match = Tuple[List[Union[Player, Optional[MatchResult]]], List[Union[Player, Optional[MatchResult]]]]
-# class PlayerResult:
-#     player_result: Tuple[Player, Optional[MatchResult]]
-
-
-@dataclass
-class Match:
-    # player_one_result: Tuple[Player, Optional[MatchResult]]
-    # player_two_result: Tuple[Player, Optional[MatchResult]]
-    # result: []
-    match: Tuple[List[Union[Player, Optional[MatchResult]]], List[Union[Player, Optional[MatchResult]]]]
-
-    # def result(self):
-    #     return list[self.player_one_result, self.player_two_result]
-    # return self.result
-
-    # def player_one_wins(self):
-    #     pass
-    #
-    # def player_two_wins(self):
-    #     pass
-    #
-    # def draw(self):
-    #     pass
+Match = Tuple[List[Union[Player, Optional[MatchResult]]], List[Union[Player, Optional[MatchResult]]]]
+# OR
+# Match = Tuple[Tuple[Player, Optional[MatchResult]], Tuple[Player, Optional[MatchResult]]]
 
 
 @dataclass
 class Round:
+    # un match = ((player1, win), (player2, loss))  OU si le match n'est pas joué (player1, None), (player2, None)
+    # 1er round, matches = [match(p1, p5), match(p2, p6), match(p3, p7), match(p4, p8)]
+    # players -> reorder with their score
+    # 2e round, matches = [match(p1, p2), match(p3, p4), match(p5, p6), match(p7, p8)]
     matches: List[Match]
     name: str
     start: datetime = field(default_factory=datetime.now)
@@ -77,6 +74,7 @@ class Round:
     end: Optional[datetime] = None
 
     '''Qui doit être automatiquement rempli lorsque l'utilisateur le marque comme terminé'''
+
     def close(self):
         self.end = datetime.now()
 
@@ -98,7 +96,12 @@ class Tournament:
     place: str
     time_control: TimeControlKind
     description: str
-    players: Dict[int, List]
+    players: Dict[int, Player]  # {1: Player1}
+    scores: Dict[Player, float]  # {Player1: 3.5}
     start: date = field(default_factory=date.today)
-    turn_count: int = DEFAULT_TURN_COUNT
-    round: List[Round] = field(default_factory=lambda: [])
+    round_count: int = DEFAULT_TURN_COUNT
+
+    # rounds = [round1, round2, ...]
+    # current round = rounds[-1]
+    # max(len(rounds)) = turn_count
+    rounds: List[Round] = field(default_factory=lambda: [])
